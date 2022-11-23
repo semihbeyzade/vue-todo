3159f92a13135c3726a05c65030e9353a8004723
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-11-23 21:20:22 +0100

    initial commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div >
+    <h1>{{ message }}</h1>
+    <h1>{{ count }}</h1>
+    <button v-on:click="countUp">Hochzählen</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoApp',
+  data() {
+    return {
+      message: "Hello World",
+      count: 0,
+    }
+  },
+  methods: {
+    countUp() {
+      this.count++;
+    },
+  },
+}
+</script>
+
